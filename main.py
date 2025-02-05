162d2b2f1b66e2351a4d91d30d173bbd40b0d46d
viorel.lazar <viorel.lazar@hotmail.com>
2025-02-05 13:55:09 +0000

    final

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean, select
 
+TopSecretAPIKey = "N6YpihjkPmdf>3U?ZY8#-9_DTj,2awoU}-Jfc.vWJfaToLdMnBKV0:9VWV5b"
 
 app = Flask(__name__)
 
@@ -101,18 +102,66 @@ def search():
         else:
             return jsonify(error={"Not found": "Sorry, we don't have a cafe at that location."}), 404
 
-
-
-
 # HTTP POST - Create Record
 
+@app.route("/add", methods=['POST'])
+def add():
+    new_entry= Cafe(
+        name = request.form.get('name'),
+        map_url = request.form.get('map_url'),
+        img_url = request.form.get('img_url'),
+        location = request.form.get('location'),
+        seats = request.form.get('seats'),
+        has_toilet = bool(request.form.get('has_toilet')),
+        has_wifi = bool(request.form.get('has_wifi')),
+        has_sockets = bool(request.form.get('has_sockets')),
+        can_take_calls = bool(request.form.get('can_take_calls')),
+        coffee_price = request.form.get('coffee_price')
+        )
+
+    db.session.add(new_entry)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
+
 # HTTP PUT/PATCH - Update Record
+
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def update_price(cafe_id):
+    new_price = request.args.get('new_price')
+
+    cafe_to_update = db.session.get(Cafe, cafe_id)
+
+
+    if cafe_to_update:
+        cafe_to_update.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the price."}), 200
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+
 
 # HTTP DELETE - Delete Record
 
 
+@app.route("/report-closed/<cafe_id>", methods=["DELETE"])
+def delete(cafe_id):
 
+    api_key = request.args.get('api-key')
 
+    if api_key == TopSecretAPIKey:
+        cafe_to_delete = db.session.get(Cafe, cafe_id)
+
+        if cafe_to_delete:
+            db.session.delete(cafe_to_delete)
+            db.session.commit()
+            return jsonify(response={"success": "Entry deleted from the database,"}), 200
+
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+
+    else:
+        return jsonify(response={"error": "Not allowed. Missing or incorrect API Key"}), 403
 
 
 if __name__ == '__main__':
